bee8f35f0938b4e0cf445e71ee3dbbda88393c46
fabian <fbngrm@gmail.com>
2024-07-18 16:19:22 +0200

    fix pipeline setup

--- src/disease_spread/pipelines/pipeline/pipeline.py
+++ src/disease_spread/pipelines/pipeline/pipeline.py
@@ -6,10 +6,10 @@ def create_pipeline(**kwargs) -> Pipeline:
     return pipeline(
         [
             node(
-                func=preprocess_data,
+                func=preprocess_data ,
                 inputs=["features", "labels"],
                 outputs=["city_sj", "city_iq"],
                 name="preprocess_data_node",
             ),
-        ]
+        ] 
     )
