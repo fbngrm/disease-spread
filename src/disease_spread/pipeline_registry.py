401af8ba205b8df7b49d118b7d71bfd4a6496368
Ni Dang <nidang1@hotmail.com>
2024-07-19 14:52:57 +0200

    fix pipeline_registry

--- src/disease_spread/pipeline_registry.py
+++ src/disease_spread/pipeline_registry.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 
 from kedro.framework.project import find_pipelines
 from kedro.pipeline import Pipeline
-
+from disease_spread.pipelines.pipeline.pipeline import create_pipeline as disease_pipeline  
 
 def register_pipelines() -> dict[str, Pipeline]:
     """Register the project's pipelines.
@@ -11,6 +11,9 @@ def register_pipelines() -> dict[str, Pipeline]:
     Returns:
         A mapping from pipeline names to ``Pipeline`` objects.
     """
-    pipelines = find_pipelines()
-    pipelines["__default__"] = sum(pipelines.values())
-    return pipelines
+    #pipelines = find_pipelines()
+    #pipelines["__default__"] = sum(pipelines.values())
+    #return pipelines
+    return {
+        "__default__": disease_pipeline()
+    }
